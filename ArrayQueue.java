78a0b46143e48fafc4ec2383c0ec1c3513121405
jukBae <zkwldkek@naver.com>
2014-11-08 04:36:37 +0900

    Remove and Modify files
    Remove Linked files, Modify Array files and Testcode file
    Add interface files

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,39 +1,22 @@
-class ArrayQueue {
-  private int front;
-  private int rear;
-  private int cnt;
-  private int size;
-  int[] node;
+import java.util.ArrayList;
+
+class ArrayQueue<T> implements Queue<T>{
+  private ArrayList<T> _node;
 
   public ArrayQueue() {
-    front = rear = cnt = 0;
-    size = 5;
-    node = new int[size];
-  }
-  public ArrayQueue(int _size) {
-    front = rear = cnt = 0;
-    size = _size;
-    node = new int[size];
-  }
-  public void enqueue(int _node) throws Exception {
-    if ( cnt < size ) {
-      node[rear] = _node;
-      rear = (rear+1)%size;
-      cnt++;
-    }
-    else {
-      throw new Exception();
-    }
+    _node = new ArrayList<T>();
   }
 
-  public int dequeue() throws Exception {
-    int ret;
+  public void enqueue(T node) {
+      _node.add(node);
+  }
 
-    if ( cnt > 0 ) {
-      ret = node[front];
-      node[front] = 0;
-      front = (front+1)%size;
-      cnt--;
+  public T dequeue() throws Exception {
+    T ret;
+
+    if ( !(_node.isEmpty()) ) {
+      ret = _node.get(0);
+      _node.remove(0);
       return ret;
     }
     else {
@@ -41,70 +24,15 @@ class ArrayQueue {
     }
   }
 
-  public int peek() throws Exception {
-    if ( cnt > 0 ) {
-      return node[front];
+  public T peek() throws Exception {
+    T ret;
+
+    if ( !(_node.isEmpty()) ) {
+      ret = _node.get(0);
+      return ret;
     }
     else {
       throw new Exception();
     }
   }
-
-  public static void main(String[] args) {
-    ArrayQueue test = new ArrayQueue(6);
-
-    try {
-      for ( int i = 0 ; i < 7; i++ ) {
-        test.enqueue(i);
-      }
-    } catch(Exception ex) {
-      System.out.println("error enqueue");
-    }
-
-    try {
-      for ( int i = 0; i < 7; i++ ) {
-        System.out.println(test.dequeue());
-      }
-    } catch(Exception ex) {
-      System.out.println("error dequeue");
-    }
-
-    try {
-      System.out.println(test.peek());
-    } catch(Exception ex) {
-      System.out.println("error peek");
-    }
-    try {
-      for ( int i = 11 ; i < 17; i++ ) {
-        test.enqueue(i);
-      }
-    } catch(Exception ex) {
-      System.out.println("error enqueue");
-    }
-
-    try {
-      for ( int i = 0; i < 3; i++ ) {
-        System.out.println(test.dequeue());
-      }
-    } catch(Exception ex) {
-      System.out.println("error dequeue");
-    }
-
-    try {
-      for ( int i = 21 ; i < 25; i++ ) {
-        test.enqueue(i);
-      }
-    } catch(Exception ex) {
-      System.out.println("error enqueue");
-    }
-
-    try {
-      for ( int i = 0; i < 7; i++ ) {
-        System.out.println(test.dequeue());
-      }
-    } catch(Exception ex) {
-      System.out.println("error dequeue");
-    }
-  }
 }
-
